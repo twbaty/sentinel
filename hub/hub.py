0517e0395b9ebdb44b675f421a5087bb97ac0932
twbaty <51901395+twbaty@users.noreply.github.com>
2025-11-23 00:38:09 -0600

    Modify MQTT client loop to run indefinitely
    Changed MQTT client loop from start to forever.

--- hub/hub.py
+++ hub/hub.py
@@ -78,15 +78,17 @@ def handle_message(topic, data):
 # ----------------------------
 
 def run_hub():
+    global mqtt_client
+
     print("[Sentinel] Starting hub...")
 
-    global mqtt_client
     mqtt_client = mqtt.Client()
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
 
     mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
-    mqtt_client.loop_start()
+
+    mqtt_client.loop_forever()
 
 if __name__ == "__main__":
     run_hub()
